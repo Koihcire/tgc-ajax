b02778d0c114b9c5ade6a2c9f49fbe1452304bdd
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-05-26 15:53:45 +0800

    q4

--- hands-on/q4-simple-json-example/script.js
+++ hands-on/q4-simple-json-example/script.js
@@ -1,21 +1,24 @@
+//CREATE ELEMENT METHOD
 document.querySelector('#load-image-btn').addEventListener('click', function(){
     async function run(){
+        //read from artwork.json for data
         let response = await axios.get('artwork.json');
         
-        let divElement = document.createElement('div');
-
+        //create h1 child <h1 class="title"></h1>
         let h1Element = document.createElement('h1');
         h1Element.className = "title";
         h1Element.innerHTML = `${response.data.title}`;
         
+        //create img child <img src="" alt="">
         let imgElement = document.createElement('img');
-        imgElement.src = `${response.data.image_url}`;
+        imgElement.src = response.data.image_url;
         
+        //put div into main div
+        let divElement = document.querySelector('#main')
+
+        //append h1+child under div
         divElement.appendChild(h1Element);
         divElement.appendChild(imgElement);
-
-        document.querySelector('#main').appendChild(divElement);
     }
     run();
 })
-
