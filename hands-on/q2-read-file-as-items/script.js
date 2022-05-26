95d0fbddb4954222d0ec365ff5b2bc835aab484b
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-05-26 19:56:54 +0800

    q2 done

---
+++ hands-on/q2-read-file-as-items/script.js
@@ -0,0 +1,27 @@
+document.querySelector("#seeFruits").addEventListener("click", async function(){
+    let items = await axios.get("items.txt");
+    let fruits = items.data;
+    console.dir(fruits);
+
+    //convert fruits into array
+    let fruitsArr = fruits.split(",");
+    console.dir(fruitsArr)
+    
+    //create a ul element
+    let ulElement = document.createElement("ul");
+    
+    //assign fruits div element and initialise empty
+    let fruitsDiv = document.querySelector("#fruits");
+    fruitsDiv.innerHTML = "";
+
+    //loop thru array
+    for (f of fruitsArr){
+        //create new li element for each item in array
+        let liElement = document.createElement("li");
+        liElement.innerHTML = f;
+        ulElement.appendChild(liElement)
+    }
+    
+    //append fruits div
+    fruitsDiv.appendChild(ulElement);
+})
