de754cad9d09e8b6d426f46d4b13e8cb1a23867b
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-05-27 19:11:36 +0800

    q6

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,7 +1,11 @@
 let url = "https://programming-quotes-api.herokuapp.com/quotes/random";
 
 document.querySelector("#load").addEventListener("click", async function () {
-    let a = await axios.get(url);
-    
-    document.querySelector("#quotes").innerHTML = a.data;
+    let response = await axios.get(url);
+    let responseData = response.data;
+    console.log(responseData);
+    document.querySelector("#quotes").innerHTML = `
+    <h3>"${responseData.en}"</h3><h4>${responseData.author}</h5>
+    `
+    //
 })
